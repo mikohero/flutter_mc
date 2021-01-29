431d9f70c360bfeb4645cd0067708674c5601bb1
Michael Koch <mikommd@gmail.com>
2021-01-29 18:58:21 +0100

    Add files via upload

---
+++ config1.php
@@ -0,0 +1,10 @@
+<?php
+$username="databaseUserName";//change username 
+$password="databasePassword"; //change password
+$host="MySqlServerURL";
+$db_name="MySQLDatabaseName"; //change databasename
+
+$connect=mysqli_connect($host,$username,$password,$db_name);
+
+
+?>
